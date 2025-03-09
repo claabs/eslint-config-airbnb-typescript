4bb044be6d3ffeae87ffee1d43bc2a4ef25738f5
Charlie Laabs <github@a.charlielaabs.com>
2025-03-09 14:34:05 -0500

    fix: react legacy config. Add name fields to flat

--- index.js
+++ index.js
@@ -1,42 +1,20 @@
-const { FlatCompat } = require('@eslint/eslintrc');
-const { convertConfigs } = require('./lib/convert-configs');
 const sharedFlat = require('./lib/shared-flat');
+const reactFlat = require('./lib/react-flat');
+const reactConfig = require('./lib/react');
 
-const compat = new FlatCompat();
-
-const reactConfig = {
-  settings: {
-    // Append 'ts' extensions to Airbnb 'import/resolver' setting
-    // Prepend 'mjs' to match shared config
-    // Original: ['.js', '.jsx', '.json']
-    'import/resolver': {
-      node: {
-        extensions: ['.mjs', '.js', '.jsx', '.json', '.ts', '.tsx', '.d.ts'],
-      },
-    },
-  },
-  rules: {
-    // Append 'tsx' to Airbnb 'react/jsx-filename-extension' rule
-    // Original: ['.jsx']
-    'react/jsx-filename-extension': ['error', { extensions: ['.jsx', '.tsx'] }],
-  },
-};
-
-const legacyReactConfig = {
+module.exports = {
   extends: ['./lib/shared'].map((path) => require.resolve(path)),
   ...reactConfig,
 };
 
-const flatReactConfig = [...convertConfigs(compat.extends('airbnb')), ...sharedFlat, reactConfig];
-
-const flatBaseConfig = [...convertConfigs(compat.extends('airbnb-base')), ...sharedFlat];
-
-const flatConfigs = {
-  base: flatBaseConfig,
-  react: flatReactConfig,
+const configs = {
+  base: sharedFlat,
+  react: reactFlat,
 };
 
-module.exports = {
-  ...legacyReactConfig, // This file adds some React specific settings. Not using React? Use base.js instead.
-  configs: flatConfigs,
-};
+Object.defineProperty(module.exports, 'configs', {
+  value: configs,
+  configurable: false,
+  enumerable: false,
+  writable: false,
+});
